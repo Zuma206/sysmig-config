fef11bda1fafb4cc348ba98fc5a97e7bc5c218f3
Zuma206 <zuma@zuma.eu.org>
2025-07-08 17:45:39 +0100

    Configurated foot font rendering

--- system.lua
+++ system.lua
@@ -1,6 +1,9 @@
 local std = require "@std"
 
 return std.system {
+  std.dnf.packages {
+    "cascadia-code-nf-fonts"
+  },
   std.symlinks {
     ["~/.config/sway"] = "sway",
     ["~/.config/kanshi"] = "kanshi",

e155a555c3ed0ddeabddb8e0b15f6202e1ad4968
Zuma206 <zuma@zuma.eu.org>
2025-07-08 15:52:18 +0100

    Added default distro waybar config

--- system.lua
+++ system.lua
@@ -4,6 +4,7 @@ return std.system {
   std.symlinks {
     ["~/.config/sway"] = "sway",
     ["~/.config/kanshi"] = "kanshi",
+    ["~/.config/waybar"] = "waybar",
     ["~/Pictures/wallpapers"] = "wallpapers"
   }
 }

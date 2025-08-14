408d9b18826033b6e41eb29e23382a09dd5d7d47
Zuma206 <zuma@zuma.eu.org>
2025-08-15 00:01:54 +0100

    Added telescope as select UI

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,12 @@
 return {
-  'nvim-telescope/telescope.nvim',
+  "nvim-telescope/telescope.nvim",
   tag = '0.1.8',
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
+    "nvim-lua/plenary.nvim",
   },
   config = function()
+    require("telescope").load_extension("ui-select")
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>b", builtin.buffers, { silent = true, noremap = true })
     vim.keymap.set("n", "<leader>f", builtin.find_files, { silent = true, noremap = true })

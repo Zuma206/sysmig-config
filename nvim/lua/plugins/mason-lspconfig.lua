104ca7b87471c3783e09cb96be60c546c1fda253
Zuma206 <zuma@zuma.eu.org>
2025-08-26 19:01:15 +0100

    Added new lsps

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -4,18 +4,33 @@ return {
     { "mason-org/mason.nvim", opts = {} },
     "neovim/nvim-lspconfig",
     "hrsh7th/cmp-nvim-lsp",
+    "b0o/schemastore.nvim",
   },
   config = function()
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local schemastore = require("schemastore")
 
     vim.lsp.config("*", {
-      capabilities = cmp_nvim_lsp.default_capabilities()
+      capabilities = cmp_nvim_lsp.default_capabilities(),
+    })
+
+    vim.lsp.config("jsonls", {
+      settings = {
+        json = {
+          schemas = schemastore.json.schemas(),
+          validate = {
+            enable = true,
+          },
+        },
+      },
     })
 
     require("mason-lspconfig").setup({
       ensure_installed = {
+        "jsonls",
         "lua_ls",
         "gopls",
+        "ts_ls",
       }
     })
   end

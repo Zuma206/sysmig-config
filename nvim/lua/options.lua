97630849d3b2e44a5f1ffc641bb5d52da9170e62
Zuma206 <zuma@zuma.eu.org>
2025-11-26 11:53:12 +0000

    Added auto formatting through conform

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -9,13 +9,6 @@ vim.opt.expandtab = true
 vim.opt.signcolumn = "yes"
 vim.o.guicursor = "n:block,i:block-blinkoff1-blinkon1"
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end,
-})
-
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.hover)
 vim.keymap.set("n", "<leader>t", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
